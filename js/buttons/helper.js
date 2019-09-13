184a2f9c350e2653629ece20a12b45a8ea4d33b2
Arjay Q. Angeles <aqangeles@gmail.com>
2019-09-13 16:28:49 +0800

    Make build url helper global.

--- js/buttons/helper.js
+++ js/buttons/helper.js
@@ -1,4 +1,4 @@
-let _buildUrl = function (dt, action) {
+window._buildUrl = function (dt, action) {
     let url = dt.ajax.url() || '';
     let params = dt.ajax.params();
     params.action = action;
