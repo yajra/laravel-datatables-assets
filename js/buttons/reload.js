ccf31e13042f97abf6f137703b198fc1d671f5ca
Arjay Q. Angeles <aqangeles@gmail.com>
2019-09-18 13:35:30 +0800

    Add reload button animation.

--- js/buttons/reload.js
+++ js/buttons/reload.js
@@ -10,6 +10,13 @@ $.fn.dataTable.ext.buttons.reload = {
     className: 'btn-primary',
     text: '<i class="fa fa-sync" data-toggle="tooltip" data-title="Reload"></i>',
     action: function (e, dt, button, config) {
+        button.find('i').removeClass('fa-sync').addClass('fa-spinner fa-spin');
+        button.attr('disabled', true);
+        dt.on('xhr.reload', () => {
+            button.find('i').addClass('fa-sync').removeClass('fa-spinner fa-spin');
+            button.attr('disabled', false);;
+        });
+
         dt.draw(false);
     }
 };
