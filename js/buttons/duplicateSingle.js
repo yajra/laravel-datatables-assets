d5e96b018709518eb6b10535520cc6b502a9df46
Arjay Q. Angeles <aqangeles@gmail.com>
2020-06-18 12:52:38 +0800

    Allow formTitle and formButtons from configuration.

--- js/buttons/duplicateSingle.js
+++ js/buttons/duplicateSingle.js
@@ -15,8 +15,8 @@ $.fn.dataTable.ext.buttons.duplicateSingle = {
         // Start in edit mode, and then change to create
         let editor = config.editor || dt.editor();
         editor.edit(dt.rows({selected: true}).indexes(), {
-            title: 'Duplicate Record',
-            buttons: [
+            title: config.formTitle || 'Duplicate Record',
+            buttons: config.formButtons || [
                 {
                     text: '<i class="fa fa-copy"></i> Duplicate',
                     className: 'btn btn-success btn-editor-duplicate',
