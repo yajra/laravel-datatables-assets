50d4589532a06ea844e5a88ed1b5d48fbd727a90
Arjay Q. Angeles <aqangeles@gmail.com>
2021-03-16 18:56:37 +0800

    Enhance reload spinner.

--- js/buttons/reload.js
+++ js/buttons/reload.js
@@ -11,13 +11,19 @@ $.fn.dataTable.ext.buttons.reload = {
     className: 'btn-primary',
     text: '<i class="fa fa-sync" data-toggle="tooltip" data-title="Reload"></i>',
     action: function (e, dt, button, config) {
-        button.find('i').removeClass('fa-sync').addClass('fa-spinner fa-spin');
-        button.attr('disabled', true);
-        dt.on('xhr.reload', () => {
-            button.find('i').addClass('fa-sync').removeClass('fa-spinner fa-spin');
-            button.attr('disabled', false);;
-        });
-
         dt.draw(false);
+    },
+    init: function (dt, node, config) {
+        dt.on('processing.dt', (e, settings, processing) => {
+            let button = $(node);
+
+            if (processing) {
+                button.find('i').removeClass('fa-sync').addClass('fa-spinner fa-spin');
+            } else {
+                button.find('i').addClass('fa-sync').removeClass('fa-spinner fa-spin');
+            }
+
+            button.attr('disabled', processing);;
+        });
     }
 };
